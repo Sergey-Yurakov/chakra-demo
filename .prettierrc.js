3dd808cb6a9f48fa4182b3e0f4f57921e737bbcd
Sergey Yurakov <barista54@yandex.ru>
2023-11-04 23:06:18 +0300

    initial project, add prettier, trivago prettier

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSpacing: true,
+    printWidth: 100,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'es5',
+    useTabs: false,
+    endOfLine: 'lf',
+
+    importOrder: ['^components/(.*)$', '^[./]'],
+
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+};
